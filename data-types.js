224fbb31a9110e230a9a952995d6499ed15d2b33
Soykot Alam <a.k.mshahalam68@gmail.com>
2023-03-25 23:53:51 +0600

    first commit

---
+++ data-types.js
@@ -0,0 +1,5 @@
+// let a = 33 ;
+// console.log(typeof a);
+
+let a = 'jamal uddin bhuia';
+console.log(typeof a)
---
+++ primitive.js
@@ -0,0 +1,14 @@
+let a = 'hello';
+let b = a;
+// console.log(a,b);
+a = 'gello';
+// console.log(a,b)
+
+const x = {job: 'Web Developer'}
+const y = x;
+console.log(x,y);
+// x.job = 'Front end developer';
+y.job = 'front end developer'
+console.log(x,y);
+
+
