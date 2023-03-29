868277afc78ba119f8f899f347ecef997a9659ae
Soykot Alam <a.k.mshahalam68@gmail.com>
2023-03-29 23:23:56 +0600

    today

--- double-triple.js
+++ double-triple.js
@@ -1,8 +1,21 @@
 const first = 2;
-const second = 2;
+const second = true;
 if(first == second){
     console.log('condition is true');
 }
 else{
     console.log('condition is false')
+}
+
+// more comparison 
+
+const a = {name: "ali"};
+const b ={name: 'ali'};
+
+
+if(a==b){
+    console.log('both are same')
+}
+else{
+    console.log("they are not same")
 }
---
+++ scope.js
@@ -0,0 +1,13 @@
+
+const fevNumber = 27;
+function add(first, second){
+    const result = first + second;
+    console.log(fevNumber);
+    console.log(result)
+    return result;
+    
+}
+
+const sum = add(11,35);
+
+
